e0320fb5d98bb8a5b97d99fd1c26bc8eabdb7bd2
Peter Jung <admin@ptr1337.dev>
2026-01-18 16:33:34 +0100

    partition: ehance error text a bit
    Signed-off-by: Peter Jung <admin@ptr1337.dev>

--- src/modules/partition/gui/ChoicePage.cpp
+++ src/modules/partition/gui/ChoicePage.cpp
@@ -1253,7 +1253,7 @@ ChoicePage::setupEfiSystemPartitionSelector()
         if ( !PartUtils::isEfiFilesystemRecommendedSize( efiPartition ) )
         {
             text += QStringLiteral( "<br/><font color=\"red\">" )
-                    + tr( "The EFI system partition is too small." )
+                    + tr( "The EFI system partition is too small, please use manual partition." )
                     + QStringLiteral( "</font>" );
         }
         m_efiLabel->setText( text );

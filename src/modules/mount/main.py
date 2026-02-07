0c98aa36c43c8e7f7c6d81e8fce2a2bdb949a789
Halim Zaghdoudi <halim.zag@gmx.de>
2026-02-07 15:59:51 +0100

    Update main.py

--- src/modules/mount/main.py
+++ src/modules/mount/main.py
@@ -244,7 +244,7 @@ def mount_partition(root_mount_point, partition, partitions, mount_options, moun
     # SELinux-enabled systems.
 
     os.makedirs(mount_point, exist_ok=True)
-
+    os.chmod(mount_point, 0o755)  # Ensure root/pacman can write here
     try:
         subprocess.call(['chcon', '--reference=' + raw_mount_point, mount_point])
     except FileNotFoundError as e:

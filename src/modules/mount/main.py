04d9091c3d895a3addd4d5ed2aa08e7597c3fd3e
Halim Zaghdoudi <halim.zag@gmx.de>
2026-02-12 13:08:03 +0000

    Update main.py

--- src/modules/mount/main.py
+++ src/modules/mount/main.py
@@ -312,7 +312,7 @@ def mount_partition(root_mount_point, partition, partitions, mount_options, moun
         try: # <--- You need this line!
             for s in btrfs_subvolumes:
                 if not s["subvolume"]:
-                    continue
+                    continue # better not crash here
                 sub_path = setup_dir + s["subvolume"]
                 if not os.path.exists(sub_path):
                     os.makedirs(os.path.dirname(sub_path), exist_ok=True)

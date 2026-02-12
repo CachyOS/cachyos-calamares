13baecf26a4a6b7ac8a0391ef801d145f5a2126f
Halim Zaghdoudi <halim.zag@gmx.de>
2026-02-12 14:09:37 +0000

    Update main.py

--- src/modules/mount/main.py
+++ src/modules/mount/main.py
@@ -312,8 +312,7 @@ def mount_partition(root_mount_point, partition, partitions, mount_options, moun
         try: # <--- You need this line!
             for s in btrfs_subvolumes:
                 if not s["subvolume"]:
-                    err(f"Btrfs subvolume not defined {device}",am) # relying on mount.conf 
-                    ### continue ### ignoring invalid config
+                    err(f"Btrfs subvolume not defined {device}",am) # instead of continue
                 sub_path = setup_dir + s["subvolume"]
                 if not os.path.exists(sub_path):
                     os.makedirs(os.path.dirname(sub_path), exist_ok=True)

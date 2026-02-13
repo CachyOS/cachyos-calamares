ef7f12733fee3f62e0f35b339e7cd3e6a6fad7a4
Halim Zaghdoudi <halim.zag@gmx.de>
2026-02-13 09:41:44 +0000

    Update main.py

--- src/modules/mount/main.py
+++ src/modules/mount/main.py
@@ -357,7 +357,7 @@ def mount_partition(root_mount_point, partition, partitions, mount_options, moun
             # This tells Linux: "Put this specific subvolume here"
             sub_opts = f"subvol={s['subvolume']},{mount_options_string}"
         else:
-            err("subvolume not defined",am)
+            err("subvolume not defined",am) # not allowing this here
 
         # This builds the path INSIDE your new root
         sub_path = root_mount_point + s["mountPoint"]

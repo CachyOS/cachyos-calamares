cd31bf5f6a1efdff7713a678aafe3710d3f7d08f
Halim Zaghdoudi <halim.zag@gmx.de>
2026-02-14 10:55:50 +0000

    Update main.py
    Validation Loop: Catches "dirty" filesystems before writing.
    
    Execution Loop: Runs only if system is clean.

--- src/modules/mount/main.py
+++ src/modules/mount/main.py
@@ -318,12 +318,14 @@ def mount_partition(root_mount_point, partition, partitions, mount_options, moun
         if libcalamares.utils.mount(device, setup_dir, fstype, "defaults") != 0:
             err(f"Cannot mount btrfs for subvolume creation {device}",am)
         try: # <--- You need this line!
-            for s in btrfs_subvolumes:
+            for s in btrfs_subvolumes: # 1. Pre-validation: Is the coast clear?
                 if not s["subvolume"]:
                     err(f"Btrfs subvolume not defined {device}",am) # instead of continue
                 sub_path = setup_dir + s["subvolume"]
                 if os.path.exists(sub_path): # if this exists user is at fault
                     err(f"Subvolume {s['subvolume']} already exists. Please format the partition to avoid a messy installation.", am)
+            for s in btrfs_subvolumes: # 2. Execution: All checks passed ("Luft ist rein")
+                sub_path = setup_dir + s["subvolume"]
                 os.makedirs(os.path.dirname(sub_path), exist_ok=True)
                 subprocess.check_call(["btrfs", "subvolume", "create", sub_path])
                 if s["mountPoint"] == "/root":

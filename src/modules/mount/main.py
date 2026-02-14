d294133ffbbff4b26fa4e54aedbac82ee57179ae
Halim Zaghdoudi <halim.zag@gmx.de>
2026-02-14 13:27:28 +0000

    Update main.py

--- src/modules/mount/main.py
+++ src/modules/mount/main.py
@@ -313,7 +313,7 @@ def mount_partition(root_mount_point, partition, partitions, mount_options, moun
                 ]
             contents = [f for f in os.listdir(mount_point) if f not in ignored_metadata]
             if contents:
-                err(f"Partition {device} has data. Please format or use /home or /srv.", am)
+                err(f"Device {device} at {raw_mount_point} has data. Please format or use /home or /srv.", am)
 
         return
 

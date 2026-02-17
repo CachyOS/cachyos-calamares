c57e42bf45cb64a9e7b85412a71c6636f1c309d6
Halim Zaghdoudi <halim.zag@gmx.de>
2026-02-17 11:37:11 +0000

    Update main.py

--- src/modules/mount/main.py
+++ src/modules/mount/main.py
@@ -282,7 +282,7 @@ def mount_partition(root_mount_point, partition, partitions, mount_options, moun
     am.append(mount_point)
     device = partition["device"]
 
-    # Block if: not a boot path, OR fat16/ntfs/ext2/exfat
+    # Only allow fat32 on boot path
     if fstype in ["fat16", "fat32", "ntfs", "ext2", "exfat"]:
         is_boot = raw_mount_point in ["/boot", "/boot/efi"]
         if not (is_boot and fstype == "fat32"):

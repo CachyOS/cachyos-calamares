2cbf8eda0000ec255e7768bab4a0d449ad665486
Halim Zaghdoudi <halim.zag@gmx.de>
2026-02-20 20:50:02 +0100

    Update main.py

--- src/modules/mount/main.py
+++ src/modules/mount/main.py
@@ -338,9 +338,7 @@ def mount_partition(root_mount_point, partition, partitions, mount_options, moun
 
     # Store created list in global storage so it can be used in the fstab module
     libcalamares.globalstorage.insert("btrfsSubvolumes", btrfs_subvolumes)
-    # Technical Debt: Calamares' fstab module expects a generic '/' entry
-    # in mount_options_list first. It then uses this 'btrfsRootSubvolume' key
-    # to find that generic entry and inject the 'subvol=@' string later.
+    # Fstab uses btrfsRootSubvolume to inject subvol=@ into generic / entry
     libcalamares.globalstorage.insert("btrfsRootSubvolume", root_sub['subvolume'])
 
     # Mount raw partition to create subvolumes

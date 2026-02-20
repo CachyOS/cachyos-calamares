97959446c690b48bbd3a4c22dfa6ba2c524713d2
Halim Zaghdoudi <halim.zag@gmx.de>
2026-02-20 19:59:28 +0100

    Update main.py

--- src/modules/mount/main.py
+++ src/modules/mount/main.py
@@ -293,6 +293,7 @@ def mount_partition(root_mount_point, partition, partitions, mount_options, moun
         device = os.path.join("/dev/mapper", partition["luksMapperName"])
 
     if fstype == "zfs":
+        # If mountpoint is not / pacstrap missing mkinitcpio hooks 
         mount_zfs(root_mount_point, partition)
         return
 
